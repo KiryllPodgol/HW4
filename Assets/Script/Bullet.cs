696a61a08ad96bf9ceae99b7ccfe8d07d56245db
Kirill <podgol04@gmail.com>
2024-12-12 18:13:00 +0300

    Правильная реализация эффектов непосредвенно через Particle system

--- Assets/Script/Bullet.cs
+++ Assets/Script/Bullet.cs
@@ -4,7 +4,7 @@ namespace Script
 {
     public class Bullet : Projectile
     {
-        public float bulletForce = 800f;
+        public ParticleSystem impactParticlesPrefab;
         public float trailFadeTime = 0.2f;
         private TrailRenderer _trailRenderer;
 
@@ -17,11 +17,26 @@ namespace Script
         protected override void ApplyImapct(Collision collision)
         {
             base.ApplyImapct(collision);
+            if (impactParticlesPrefab != null)
+            {
+                PlayImpactParticles(collision.contacts[0].point, collision.contacts[0].normal);
+            }
+            else
+            {
+                Debug.LogWarning("impactParticlesPrefab is not assigned in the inspector.");
+            }
             if (_trailRenderer != null)
             {
                 _trailRenderer.autodestruct = true;
                 _trailRenderer.time = trailFadeTime;
             }
+        }
+
+        private void PlayImpactParticles(Vector3 position, Vector3 normal)
+        {
+            ParticleSystem impactParticles = Instantiate(impactParticlesPrefab, position, Quaternion.LookRotation(normal));
+            impactParticles.Play();
+            Destroy(impactParticles.gameObject, impactParticles.main.duration);
         }
     }
 }
